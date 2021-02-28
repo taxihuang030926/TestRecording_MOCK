6c76aad7c8923f4ac04853b1a27c91de1cbaa098
Ting Yu Huang <taxihuang030926@gmail.com>
2021-02-28 16:14:14 +0800

    added: view setups, navigations

--- app/src/main/java/com/example/testrecording_mock/RecordFragment.java
+++ app/src/main/java/com/example/testrecording_mock/RecordFragment.java
@@ -1,19 +1,28 @@
 package com.example.testrecording_mock;
 
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link RecordFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RecordFragment extends Fragment {
+public class RecordFragment extends Fragment implements View.OnClickListener {
+
+    private NavController navController;
+    private ImageView rcdListBtn;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +69,24 @@ public class RecordFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        rcdListBtn = view.findViewById(R.id.record_list_btn);
+
+        rcdListBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.record_list_btn:
+                navController.navigate(R.id.action_recordFragment_to_audioListFragment);
+                break;
+        }
     }
 }
