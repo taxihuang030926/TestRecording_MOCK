97f25387dc485b38acb45cf10fc20a4c39a60cb8
Ting Yu Huang <taxihuang030926@gmail.com>
2021-03-07 14:34:37 +0800

    added: nav animations, functions to btns, permission of recording, list adapter, list item

--- app/src/main/java/com/example/testrecording_mock/RecordFragment.java
+++ app/src/main/java/com/example/testrecording_mock/RecordFragment.java
@@ -1,68 +1,48 @@
 package com.example.testrecording_mock;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.media.Image;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class RecordFragment extends Fragment implements View.OnClickListener {
 
     private NavController navController;
-    private ImageView rcdListBtn;
+    private ImageButton rcdListBtn;
+    private ImageButton rcdBtn;
+    private TextView filenameText;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private boolean isRecording = false;
+    private int PERMISSION_CODE = 21;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String recordPermission = Manifest.permission.RECORD_AUDIO;
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
 
-    public RecordFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecordFragment newInstance(String param1, String param2) {
-        RecordFragment fragment = new RecordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Chronometer timer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -77,8 +57,12 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
 
         navController = Navigation.findNavController(view);
         rcdListBtn = view.findViewById(R.id.record_list_btn);
+        rcdBtn = view.findViewById(R.id.record_btn);
+        timer = view.findViewById(R.id.record_timer);
+        filenameText = view.findViewById(R.id.record_filename);
 
         rcdListBtn.setOnClickListener(this);
+        rcdBtn.setOnClickListener(this);
     }
 
     @Override
@@ -87,6 +71,70 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
             case R.id.record_list_btn:
                 navController.navigate(R.id.action_recordFragment_to_audioListFragment);
                 break;
+
+            case R.id.record_btn:
+                if(isRecording){
+                    //Stop Recording
+                    stopRecording();
+
+                    rcdBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn,  null));
+                    isRecording = false;
+                }else{
+                    if(checkPermissions()) {
+                        //Start Recording
+                        startRecording();
+
+                        rcdBtn.setImageDrawable(getResources().getDrawable(R.drawable.grey_sqr, null));
+                        isRecording = true;
+                    }
+                }
+                break;
+        }
+    }
+
+    private void stopRecording() {
+        timer.stop();
+
+        filenameText.setText("Record Stopped, File Saved : " + recordFile);
+
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    private void startRecording() {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.start();
+
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.TAIWAN);
+        Date now = new Date();
+
+        recordFile = "Recording..." + formatter.format(now) + ".gp3";
+
+        filenameText.setText("Recording, File Name : " + recordFile);
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath + "/" + recordFile);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    private boolean checkPermissions() {
+        if(ActivityCompat.checkSelfPermission(getContext(), recordPermission) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            ActivityCompat.requestPermissions(getActivity(), new String[]{recordPermission}, PERMISSION_CODE);
+            return false;
         }
     }
 }
