cd4be7e1d76c6f29bd03e55fbe2477e1ad9036bd
taxihuang030926 <52541932+taxihuang030926@users.noreply.github.com>
2021-04-15 13:23:10 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/testrecording_mock/bottom_nav_bar.java
@@ -0,0 +1,14 @@
+package com.example.testrecording_mock;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class bottom_nav_bar extends Fragment {
+
+}
